367d104634d19a0e44f684cda98e47b64f75771d
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2025-05-17 16:17:54 +0100

    feat: Modified virtualStockSocket

---
+++ components/TestFile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestFile = () => {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus explicabo omnis beatae rem, porro repellat voluptates voluptatibus non eaque eum est mollitia distinctio facilis quis laudantium laborum earum tempora quam.
+    </div>
+  )
+}
+
+export default TestFile
