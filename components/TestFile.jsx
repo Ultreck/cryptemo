ce1ffa43d41972ec13f928986861f4c327efdc3b
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2025-05-21 20:39:01 +0100

    feat: Modified README.md file

--- components/TestFile.jsx
+++ components/TestFile.jsx
@@ -5,6 +5,7 @@ const TestFile = () => {
     <div>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus explicabo omnis beatae rem, porro repellat voluptates voluptatibus non eaque eum est mollitia distinctio facilis quis laudantium laborum earum tempora quam.
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus explicabo omnis beatae rem, porro repellat voluptates voluptatibus non eaque eum est mollitia distinctio facilis quis laudantium laborum earum tempora quam.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus explicabo omnis beatae rem, porro repellat voluptates voluptatibus non eaque eum est mollitia distinctio facilis quis laudantium laborum earum tempora quam.
     </div>
   )
 }
