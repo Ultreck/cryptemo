34119f31832288edec8801fbb8576648c8a7ab5a
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2025-05-24 12:31:55 +0100

    feat: Modified virtualStockSocket

--- components/TestFile.jsx
+++ components/TestFile.jsx
@@ -6,7 +6,6 @@ const TestFile = () => {
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus explicabo omnis beatae rem, porro repellat voluptates voluptatibus non eaque eum est mollitia distinctio facilis quis laudantium laborum earum tempora quam.
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus explicabo omnis beatae rem, porro repellat voluptates voluptatibus non eaque eum est mollitia distinctio facilis quis laudantium laborum earum tempora quam.
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus explicabo omnis beatae rem, porro repellat voluptates voluptatibus non eaque eum est mollitia distinctio facilis quis laudantium laborum earum tempora quam.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus explicabo omnis beatae rem, porro repellat voluptates voluptatibus non eaque eum est mollitia distinctio facilis quis laudantium laborum earum tempora quam.
     </div>
   )
 }
